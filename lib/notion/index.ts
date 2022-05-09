d803c186b9ec17d9dc949de076dafd97be9cb131
mishitoshi <mishitoshiii@gmail.com>
2022-05-10 00:12:28 +0900

    記事の一覧取得ロジック

---
+++ lib/notion/index.ts
@@ -0,0 +1,29 @@
+import { Client } from '@notionhq/client';
+import type { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
+
+// FIXME: 型どうするか考える
+export const getPosts = async (): Promise<any> => {
+  const databaseId = process.env.NOTION_DATABASE_ID as string;
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      or: [
+        {
+          property: 'Publish',
+          checkbox: {
+            equals: true,
+          },
+        },
+      ],
+    },
+    sorts: [
+      {
+        property: 'CreatedAt',
+        direction: 'descending',
+      },
+    ],
+  });
+  return response.results;
+};
