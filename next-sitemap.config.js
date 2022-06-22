cfa989c6339e9d43e7b8e9d301aec3780e64d373
mishitoshi <mishitoshiii@gmail.com>
2022-06-22 23:23:32 +0900

    build時にsitemap生成

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://blog.mishitoshi.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
+
+module.exports = config;
